6fc10141ed33c59accbc4629024834b8d1c68d14
thays-araujo-de-oliveira <161478833+thays-araujo-de-oliveira@users.noreply.github.com>
2024-11-19 09:28:41 -0300

    Add files via upload

---
+++ rosa.book-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Espanhol',
+    'Como se diz: como vocẽ esta?, em espanhol?',
+    'Cómo estás?'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em área?',
+    'Rússia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amarelo em Inglês?',
+    'amarelo em ingles é yellow (RAI)'
+) 
